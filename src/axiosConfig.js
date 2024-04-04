d2ac1265271c9a881a8496f75bbc979e94c107c6
yonas52 <yonasabeje5@gmail.com>
2024-04-03 22:09:51 -0400

    cyclic backend server

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const axiosBase=axios.create({
     // baseURL:'http://localhost:5500/api'
-    baseURL:'https://evangadi-backend-deploy-3.onrender.com/api'
+    // baseURL:'https://evangadi-backend-deploy-3.onrender.com/api'
+    baseURL:'https://ant-singlet.cyclic.app/api'
 })
 
 export default axiosBase;
