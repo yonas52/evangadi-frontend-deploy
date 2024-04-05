9fc2686df6a490d79fcc94f54ea6fcb256c6bba4
yonas52 <yonasabeje5@gmail.com>
2024-04-05 14:34:25 -0400

    add new axiosConfig

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const axiosBase=axios.create({
     // baseURL:'http://localhost:5500/api'
     // baseURL:'https://evangadi-backend-deploy-3.onrender.com/api'
-    baseURL:'https://ant-singlet.cyclic.app/api'
+    // baseURL:'https://ant-singlet.cyclic.app/api'
+    baseURL:'https://backend.yonashabtegeorgis.com/api'
 })
 
 export default axiosBase;
